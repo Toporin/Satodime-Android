1a5ed0e1392b20aae31defb088c5208ceccdc25a
Toporin <satochip.wallet@gmail.com>
2023-06-05 21:36:55 +0100

    v0.1.2: improved boarding screens

--- satodime-android/src/main/java/org/satochip/satodimeapp/adapter/OnBoardingSliderAdapter.java
+++ satodime-android/src/main/java/org/satochip/satodimeapp/adapter/OnBoardingSliderAdapter.java
@@ -10,6 +10,7 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.PagerAdapter;
 
 import org.satochip.satodimeapp.R;
@@ -22,20 +23,21 @@ public class OnBoardingSliderAdapter extends PagerAdapter {
     LayoutInflater layoutInflater;
     public String[] slideHeadings;
     public String[] slideDescriptions;
+    public int[] slideImages;
 
-
-    public OnBoardingSliderAdapter(OnBoardingActivity context, String[] slideDescriptions, String[] slideHeadings) {
+    public OnBoardingSliderAdapter(OnBoardingActivity context, String[] slideDescriptions, String[] slideHeadings, int[] slideImages) {
         this.context=context;
         this.slideDescriptions=slideDescriptions;
         this.slideHeadings=slideHeadings;
+        this.slideImages=slideImages;
     }
 
-    public int[] slideImages = {
-            R.drawable.ic_onboarding_1,
-            R.drawable.ic_onboarding_2,
-            R.drawable.ic_onboarding_3,
-            R.drawable.onboarding_4,
-    };
+//    public int[] slideImages = {
+//            R.drawable.ic_onboarding_1,
+//            R.drawable.ic_onboarding_2,
+//            R.drawable.ic_onboarding_3,
+//            R.drawable.onboarding_4,
+//    };
 
     @Override
     public int getCount() {
