907dab1303b28a16854604b134c9973e5a816220
Petar Sebic <petarsebic@milicamartin.vega.local>
2024-05-15 10:00:53 +0200

    feature: added crashlytics and playstore in app review

--- app/src/main/java/org/satochip/satodimeapp/MainActivity.kt
+++ app/src/main/java/org/satochip/satodimeapp/MainActivity.kt
@@ -13,15 +13,26 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import org.satochip.satodimeapp.ui.components.shared.SatoToast
 import org.satochip.satodimeapp.ui.theme.SatoGreen
+import com.google.android.play.core.review.ReviewManagerFactory
 import org.satochip.satodimeapp.ui.theme.SatodimeTheme
 import org.satochip.satodimeapp.util.internetconnection.ConnectionChecker
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var connectionChecker: ConnectionChecker
+    private fun showFeedbackDialog() {
+        val reviewManager = ReviewManagerFactory.create(applicationContext)
+        reviewManager.requestReviewFlow().addOnCompleteListener {
+            if(it.isSuccessful) {
+                reviewManager.launchReviewFlow(this, it.result)
+            }
+        }
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         connectionChecker = ConnectionChecker(applicationContext)
+        showFeedbackDialog()
 
         setContent {
             SatodimeTheme {
